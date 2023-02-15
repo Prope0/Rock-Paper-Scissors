e3bc2cb3af6199872039a77aef2402bf9d639e05
Prope0~ <tamas.zsigo.92@gmail.com>
2023-02-16 00:20:39 +0100

    computer randon choice, one round and game functions added

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
 // Random choice from Computer
-computerChoices = [ "Rock" , "Paper" , "Scissors" ]
+function getComputerChoice() {
+    computerChoices = [ "rock" , "paper" , "scissors" ]
+    return computerChoices[Math.floor(Math.random()*computerChoices.length)];
+}
 
-const getComputerChoice = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+//Capitalize first letter
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-alert(getComputerChoice)
+//One round game
+function oneRound (playerSelection , computerSelection, capitalizeFirstLette) {
+    computerSelection = getComputerChoice();
+    playerSelection = prompt("Choose Your Weapon: ").toLowerCase();    
+    console.log("Player: " + playerSelection);
+    console.log("Computer: " + computerSelection);
+    //paper nyer ha cp rock 
+    //rock nyer ha cp scissors 
+    // scissors nyer ha cp paper
+    if ((playerSelection === "paper" && computerSelection === "rock" ) ||
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+        return `You win! ${playerSelection} beats ${computerSelection} `;
+        } else if (playerSelection === computerSelection) {
+            return "It is a tie!"
+        }
+        else {
+            return ` You lose! ${computerSelection} beats ${playerSelection}`
+        }
+}
+
+
+/*
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(oneRound(playerSelection, computerSelection));
+*/
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(oneRound())
+        } 
+    }
+
+game()
+
+
+
+
