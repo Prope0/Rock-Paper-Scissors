f6b0f8c000e459f35a904632f807fd62da8706fa
Prope0~ <tamas.zsigo.92@gmail.com>
2023-02-21 22:58:42 +0100

    dont know anything...

--- main.js
+++ main.js
@@ -1,51 +1,74 @@
+let playerScore = 0;
+let ComputerScore = 0;
+
 // Random choice from Computer
 function getComputerChoice() {
-    computerChoices = [ "rock" , "paper" , "scissors" ]
-    return computerChoices[Math.floor(Math.random()*computerChoices.length)];
+  computerChoices = ["rock", "paper", "scissors"];
+  return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
 //Capitalize first letter
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowercase();
 }
 
-//One round game
-function oneRound (playerSelection , computerSelection, capitalizeFirstLette) {
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Choose Your Weapon: ").toLowerCase();    
+const resultsDivJs = document.querySelector("#resultsDiv");
+const content = document.createElement("div");
+//content.textContent = `You win! ${playerSelection} beats ${computerSelection} `;
+//resultsDivJs.appendChild(content);
+
+//Three button listen to click and play a round with the choosed weapon
+const rockBtnJs = document
+  .getElementById("rockBtn")
+  .addEventListener("click", () => {
+    playerSelection = "rock";
+    oneRound(playerSelection);
     console.log("Player: " + playerSelection);
-    console.log("Computer: " + computerSelection);
-    //paper nyer ha cp rock 
-    //rock nyer ha cp scissors 
-    // scissors nyer ha cp paper
-    if ((playerSelection === "paper" && computerSelection === "rock" ) ||
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "paper")) {
-        return `You win! ${playerSelection} beats ${computerSelection} `;
-        } else if (playerSelection === computerSelection) {
-            return "It is a tie!"
-        }
-        else {
-            return ` You lose! ${computerSelection} beats ${playerSelection}`
-        }
+  });
+
+const paperBtnJs = document
+  .getElementById("paperBtn")
+  .addEventListener("click", () => {
+    playerSelection = "paper";
+    oneRound(playerSelection);
+    console.log("Player: " + playerSelection);
+  });
+
+const scissorsBtnJs = document
+  .getElementById("scissorsBtn")
+  .addEventListener("click", () => {
+    playerSelection = "scissors";
+    oneRound(playerSelection);
+    console.log("Player: " + playerSelection);
+  });
+
+//One round of game
+function oneRound(playerSelection, playerScore) {
+  computerSelection = getComputerChoice();
+  console.log("Computer: " + computerSelection);
+  if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore += 1;
+    console.log(playerscore);
+  } else if (playerSelection === computerSelection) {
+    return "It is a tie!";
+  } else {
+    return ` You lose! ${computerSelection} beats ${playerSelection}`;
+  }
 }
 
 
 /*
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(oneRound(playerSelection, computerSelection));
-*/
 
-
+// play 5 rounds
 function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(oneRound())
-        } 
-    }
-
-game()
-
-
-
-
+  for (let i = 0; i < 5; i++) {
+    console.log(oneRound());
+  }
+}
+// start the game
+game();
+*/
